eab7ded65b62ffa6f40373e477e6308dcfd9d196
Paul Bailey <paul@neutron.studio>
2022-01-12 17:13:26 -0600

    added bypass and prefix option

--- shelix/main.py
+++ shelix/main.py
@@ -10,9 +10,12 @@ import subprocess
 import time
 import traceback
 
-import typer
+
+from haikunator import Haikunator
 
 import asynchronousfilereader as asfr
+import typer
+
 
 from shelix.log_socket import start_socket
 
@@ -64,8 +67,14 @@ def main(
         command: str,
         log_id: str = typer.Argument("Log ID", envvar='SHELIX_LOGID'),
         token: str = typer.Argument("API Token", envvar='SHELIX_TOKEN'),
-        ws_url: str = typer.Argument("Server URL", envvar='SHELIX_WS_URL')
+        ws_url: str = typer.Argument("Server URL", envvar='SHELIX_WS_URL'),
+        bypass: bool = typer.Option(False, help="Bypass Reporting to the server", envvar="SHELIX_BYPASS"),
+        prefix: str = typer.Option('', help="Prefix log output", envvar="SHELIX_PREFIX"),
     ):
+
+    if prefix:
+        haikunator = Haikunator()
+        prefix += '-' + '-'.join(haikunator.haikunate(token_length=3).split('-')[1:]) + ': '
 
     q = queue.Queue(maxsize=1024 * 1024)
 
@@ -85,22 +94,29 @@ def main(
     last_send = datetime.datetime.utcnow()
     content = ''
 
-    ws_url = f'{ws_url}/?token={token}'
-    ws = start_socket(ws_url)
+    if bypass:
+        print('Bypassing log send to server')
+        print('prefix', prefix)
+
+    else:
+        ws_url = f'{ws_url}/?token={token}'
+        ws = start_socket(ws_url)
 
     while not reader.eof():
         while not q.empty():
             line = q.get()
             if line:
-                line = line.decode()
+                line = prefix + line.decode()
                 print(line, end='')
-                content += line
-                content, last_send = send_lines(ws, log_id, content, last_send)
+                if not bypass:
+                    content += line
+                    content, last_send = send_lines(ws, log_id, content, last_send)
 
         time.sleep(0.1)
 
-    send_lines(ws, log_id, content, last_send, force=True)
-    ws.close()
+    if not bypass:
+        send_lines(ws, log_id, content, last_send, force=True)
+        ws.close()
 
 
 def run_cli():
